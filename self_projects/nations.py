0b622f5f7ee5c402a5652f490a105ccb24596ec5
flumph3927 <levi.morris@ucas-edu.net>
2026-01-14 15:59:35 +0000

    nat

--- self_projects/nations.py
+++ self_projects/nations.py
@@ -1,28 +1,29 @@
 import random
 
-z=45
-y=30
+z=60
+y=40
 mp=[]
 for i in range(y):
     mp.append([])
     for x in range(z):
-        mp[i].append('')
+        mp[i].append([''])
 
 dne=False
 while not dne:
     tle=[random.randint(0,(y-1)),random.randint(0,(z-1))]
-    if mp[tle[0]][tle[1]]=='':
+    if mp[tle[0]][tle[1]][0]=='':
         ps=['~','#']
         for j in [[tle[0],tle[1]+1],[tle[0]-1,tle[1]],[tle[0]+1,tle[1]],[tle[0],tle[1]-1]]:
             if j[0]<(y-1) and j[0]>-1 and j[1]>-1 and j[1]<(z-1):
-                if mp[j[0]][j[1]] != '':
-                    for a in range(100):
-                        ps.append(mp[j[0]][j[1]])
-        mp[tle[0]][tle[1]]=random.choice(ps)
+                if mp[j[0]][j[1]][0] != '':
+                    for i in range(100):
+                        ps.append(mp[j[0]][j[1]][0])
+        while mp[tle[0]][tle[1]][0] == '':
+            mp[tle[0]][tle[1]][0]=random.choice(ps)
     dne=True
     for i in mp:
         for x in i:
-            if x == '':
+            if x[0] == '':
                 dne=False
 
 for i in range(100):
@@ -38,14 +39,14 @@ for i in range(100):
                     if not j[1]<z:
                         j[1]-=z
                     for a in range(10):
-                        ps.append(mp[j[0]][j[1]])
+                        ps.append(mp[j[0]][j[1]][0])
                 for b in range(100):
-                    ps.append(mp[i][x])
-                mp[tle[0]][tle[1]]=random.choice(ps)
+                    ps.append(mp[i][x][0])
+                mp[tle[0]][tle[1]][0]=random.choice(ps)
         dne=True
         for i in mp:
             for x in i:
-                if x == '':
+                if x[0] == '':
                     dne=False
 
 for i in range(1):
@@ -53,8 +54,9 @@ for i in range(1):
     while not dne:
         for i in range(y):
             for x in range(z):
-                if mp[i][x]!='~':
+                if mp[i][x][0]!='~':
                     tle=[i,x]
+                    mp[i][x].append(['-','^','+','&','%','!','*'])
                     ps=['-','^','+','&','%','!','*']
                     for j in [[tle[0],tle[1]+1],[tle[0]-1,tle[1]],[tle[0]+1,tle[1]],[tle[0],tle[1]-1]]:
                         if not j[0]<y:
@@ -62,15 +64,15 @@ for i in range(1):
                         if not j[1]<z:
                             j[1]-=z
                         for a in range(100):
-                            if mp[j[0]][j[1]] not in ['~','#']:
-                                ps.append(mp[j[0]][j[1]])
+                            if mp[j[0]][j[1]][0] not in ['~','#']:
+                                ps.append(mp[j[0]][j[1]][0])
                     for b in range(100):
-                        ps.append(mp[i][x])
-                    mp[tle[0]][tle[1]]=random.choice(ps)
+                        ps.append(mp[i][x][0])
+                    mp[tle[0]][tle[1]][0]=random.choice(ps)
         dne=True
         for i in mp:
             for x in i:
-                if x == '#':
+                if x[0] == '#':
                     dne=False
 
 for i in range(1500):
@@ -78,24 +80,26 @@ for i in range(1500):
     while not dne:
         for i in range(y):
             for x in range(z):
-                if mp[i][x]!='~':
+                mp[i][x][1].append(mp[i][x][0]) #FIX THIS  MOVE DOWN
+                if mp[i][x][0]!='~':
                     tle=[i,x]
-                    ps=['-','^','+','&','%','!','*']
+                    ps=mp[i][x][1]
                     for j in [[tle[0],tle[1]+1],[tle[0]-1,tle[1]],[tle[0]+1,tle[1]],[tle[0],tle[1]-1]]:
                         if not j[0]<y:
                             j[0]-=y
                         if not j[1]<z:
                             j[1]-=z
                         for a in range(100):
-                            if mp[j[0]][j[1]]!='~':
-                                ps.append(mp[j[0]][j[1]])
+                            if mp[j[0]][j[1]][0]!='~':
+                                ps.append(mp[j[0]][j[1]][0])
                     for b in range(100):
-                        ps.append(mp[i][x])
-                    mp[tle[0]][tle[1]]=random.choice(ps)
+                        ps.append(mp[i][x][0])
+                    mp[tle[0]][tle[1]][0]=random.choice(ps)
+                
         dne=True
         for i in mp:
             for x in i:
-                if x == '':
+                if x[0] == '':
                     dne=False
 
     mappp=''
@@ -103,6 +107,6 @@ for i in range(1500):
     cds={'~':'\033[104m','-':'\033[43m','^':'\033[1m\033[47m','+':'\033[102m','&':'\33[45m','%':'\33[40m','!':'\33[41m','*':'\33[46m','#':''}
     for i in mp:
         for x in i:
-            mapp+=(cds[x]+' '+x+' '+'\033[00m')
+            mapp+=(cds[x[0]]+' '+x[0]+' '+'\033[00m')
         mapp+='\n'
     print(mapp,end='\n')
