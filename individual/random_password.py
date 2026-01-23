b9f14c48709cf29cb4eaa26882a021a62a6d56ef
flumph3927 <levi.morris@ucas-edu.net>
2026-01-23 16:51:40 +0000

    rand

--- individual/random_password.py
+++ individual/random_password.py
@@ -20,4 +20,13 @@
     #loop amount times
         #add randomly chosen special character to OUT
 
-#create function distribute, getting
+#create function distribute, get LENGTH and CHOSEN
+    #create dictionary, 4 items, keys upper, lower, number, special, values all 0
+    #loop LENGTH minus length of CHOSEN times
+        #randomly choose from CHOSEN list, add 1 to appropriate item in dictionary
+    #add 1 to all items in dictionary that correspond with items in CHOSEN
+
+#create function main
+    #display welcome to the random password generator
+    #loop
+        #
