ba5e76d0fd72ed08062daf194ddfc1a2b01eb0e9
flumph3927 <levi.morris@ucas-edu.net>
2026-02-19 09:50:27 -0700

    files and start timestamp

--- individual/word_counter/files.py
+++ individual/word_counter/files.py
@@ -1,21 +1,36 @@
 #LM 1st Word Counter
 
 #create function get words, get file path
+def get_words(path):
     #open file to read
+    with open(path, 'r') as file:
+        content=file.read()
     #get information from file
     #split into list of words
+    words=content.split()
     #return length of list of words
+    return len(words)
 
 #create function add info, get time and file path, word count
+def add_info(path,time,count):
     #open file to write
+    with open(path, 'a') as file:
+        file.write(f'\nWord Count: {count}\nTime: {time}\n')
     #write word count and time to file
 
 #create function write line, get file path
+def write_line(path):
     #get user input for line
+    line=input('Line to write: ')
     #open file to write
+    with open(path, 'a') as file:
     #write line to file
+        file.write(line)
 
 #create function reading, get file path
+def readin(path):
     #open file to read
+    with open(path,'r') as file:
         #display lines in order
-
+        content=file.read()
+    print(content)
--- individual/word_counter/times.py
+++ individual/word_counter/times.py
@@ -1,6 +1,9 @@
 #LM 1st Word Counter
+import datetime
 
-#create function get_time
+#create function get time
+def get_time():
     #get timestamp
+    stamp=datetime.datetime.now()
     #reformat time
     #return time
