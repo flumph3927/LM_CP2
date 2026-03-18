4da7852e9b5bf8cf663a5964399ab08b6967b7ad
flumph3927 <levi.morris@ucas-edu.net>
2026-03-18 15:52:20 -0700

    law of cosines!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- individual/classes_project/src/2d.py
+++ individual/classes_project/src/2d.py
@@ -1,6 +1,5 @@
 #2d shape classes file
-import turtle
-
+import turtle, math
 
 #create class circ
 class circ:
@@ -77,14 +76,14 @@ class rect:
         self.name=name
 
     #string method
-    def __str__(self)
+    def __str__(self):
         #display all rect attributes
         return f'''Name: {self.name}
 Type: {self.__class__.__name__}
 Width: {self.x}
 Height: {self.y}
 Area: {self.area}
-Perimiter: {self.perimiter}
+Perimeter: {self.perimiter}
 '''
 
     #draw method
@@ -98,24 +97,77 @@ Perimiter: {self.perimiter}
             turtle.forward(self.x*50)
             turtle.lt(90)
             turtle.forward(self.y*50)
+            turtle.lt(90)
         #hold turtle screen
+        turtle.done()
 
     #info method
+    def info():
         #show formulas and explanation
+        print('The formula for the area of a rectangle is lw, with l being lenth and w being width.\nThe formula for the perimiter of a rectangle is 2(l+w), with l being length and w  being width.')
 
 
 #create class tri
+class tri:
     #initialize, get b, h and name
-        #set perimiter, base, height, area, stretch, rotation, shifts and name
+    def __init__(self,name):
+        #set perimiter, base, height, area, stretch, rotation, shifts and namewhile True:
+        while True:
+            try:
+                self.a=int(input('Side A of the triangle: '))
+                if self.a>0:
+                    break
+            except:
+                print('Invalid input. Try again.')
+        while True:
+            try:
+                self.b=int(input('Side B of the triangle: '))
+                if self.b>0:
+                    break
+            except:
+                print('Invalid input. Try again.')
+        while True:
+            try:
+                self.c=int(input('Side C of the triangle: '))
+                if self.c>0:
+                    break
+            except:
+                print('Invalid input. Try again.')
+        self.perimiter=self.a+self.b+self.c
+        semi=self.perimiter/2
+        self.area=math.sqrt(semi(semi-self.a)(semi-self.b)(semi-self.c))
+        self.stretch=1
+        self.rotation=0
+        self.shifts=[0,0]
+        self.name=name
 
     #string method
+    def __str__(self):
         #display all triangle attributes
+        return f'''Name: {self.name}
+Type: {self.__class__.__name__}
+Side A: {self.a}
+Side B: {self.b}
+Side C: {self.c}
+Area: {self.area}
+Perimeter: {self.perimiter}
+'''
 
     #draw method
+    def draw(self):
         #imprt turtle
         #move to implement rotation and shifts
+        turtle.teleport(self.shifts[0]*50, self.shifts[1]*50)
+        turtle.lt(self.rotation)
         #draw triangle with base base and height height and stretch
+        turtle.forward(self.a*50)
+        turtle.lt(math.acos((self.a^2+self.b^2-self.c^2)/2*self.a*self.b))
+        turtle.forward(self.b*50)
+        turtle.lt(math.acos((self.c^2+self.b^2-self.a^2)/2*self.c*self.b))
+        turtle.forward(self.c*50)
+        turtle.lt(math.acos((self.a^2+self.c^2-self.b^2)/2*self.a*self.c))
         #hold turtle screen
+        turtle.done()
 
     #info method
         #show formulas and explanation
