8a1c9fa1591ce985addcc2425fb995c998f0ab44
flumph3927 <levi.morris@ucas-edu.net>
2026-03-19 19:13:10 +0000

    aah

--- individual/classes_project/src/relations.py
+++ individual/classes_project/src/relations.py
@@ -1,13 +1,52 @@
 #comparison functions file
 
 #create function choice, get opt and objedcts
+def choice(opt,objects):
     #if opt is one, return  object with larger area
-    #else, return object with larger perimiter
+    try:
+        if opt==1:
+            if objects[0].area>objects[1].area:
+                return objects[0]
+            else:return objects[1]
+    #else, return object with larger perimeter
+        else:
+            if objects[0].perimeter>objects[1].perimeter:
+                return objects[0]
+            else:return objects[1]
+    except:
+        #for 3d
+        if opt==1:
+            if objects[0].volume>objects[1].volume:
+                return objects[0]
+            else:return objects[1]
+        else:
+            if objects[0].surface>objects[1].surface:
+                return objects[0]
+            else:return objects[1]
 
 #create function sort, get opt, objects
+def sort(opt,objects):
+    try:
     #if opt is one, return objects reordered by area
-    #else, return objects reordered by perimiter
+        if opt==1:
+            return dict(sorted(objects,key=lambda s: s[1].area))
+    #else, return objects reordered by perimeter
+        else:
+            return dict(sorted(objects,key=lambda s: s[1].perimeter))
+    #for 3d:
+    except:
+        if opt==1:
+            return dict(sorted(objects,key=lambda s: s[1].volume))
+        else:
+            return dict(sorted(objects,key=lambda s: s[1].surface))
 
 #create function select, get objects
+def select(objects):
     #loop through and show all objects
+    for i,v in enumerate(objects):
+        print(f'{i+1}: {v[1]}\n\n')
     #return selected object
+    while True:
+        try:
+            choice=int(input('Which shape would you like to select? '))
+            if choice==
--- individual/classes_project/src/transform.py
+++ individual/classes_project/src/transform.py
@@ -1,11 +1,21 @@
-#transformations function
+#transformations functions
 
 #create add rotation function, get rotation and class
+def rotate(rotation,clas):
     #set class rotation to rotation
+    clas.rotation=rotation
+    return clas
 
 #create add scale function, get scale and class
+def scale(scale,clas):
     #set class scale to scale
+    clas.stretch=scale
+    return clas
 
 #create add shifts function, get shifts([x,y]) and class
+def shifter(shifts,clas):
     #set class shift x to shift x
+    clas.shifts[0]=shifts[0]
     #set class shift y to shift y
+    clas.shifts[1]=shifts[1]
+    return clas
